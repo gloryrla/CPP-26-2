b2817c7aa9d37ed038f25cff34ab5fbf85b4505e
gloryrla <kyg030414@gmail.com>
2026-09-21 15:11:16 +0900

    practice Wee3

--- challenge/Week3-1/temperature.cpp
+++ challenge/Week3-1/temperature.cpp
@@ -5,15 +5,12 @@ using namespace std;
 int main() {
 
     double fah; //화시온도
-    double cel = (5.0/9.0) * (fah - 32); //섭씨 온도
 
-    
     cout << "화씨온도: ";
     cin >> fah;
 
-    //double cel = (5.0/9.0) * (fah - 32); //섭씨 온도
+    double cel = (5.0/9.0) * (fah - 32); //섭씨 온도
 
     cout << "섭씨 온도 = " << cel << endl;
-
 
 }
